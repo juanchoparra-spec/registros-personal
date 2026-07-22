241dbfc897cd6982ed39088cdb69981bbb3324b2
juanchoparra-spec <juanchoparra@gmail.com>
2026-07-22 19:52:29 -0400

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+// Configuración del proyecto de Firebase "Registros"
+const firebaseConfig = {
+  apiKey: "AIzaSyCopkLUKRRloOzc-yxYEGBccCgqsOVcbrE",
+  authDomain: "registros-975e6.firebaseapp.com",
+  projectId: "registros-975e6",
+  storageBucket: "registros-975e6.firebasestorage.app",
+  messagingSenderId: "676387476957",
+  appId: "1:676387476957:web:3753a003f535d413a01234"
+};
